698678d0b2333450d12e6a655187195f0ceed47b
Valentyn <valentyn@Valentyns-MacBook-Pro.local>
2020-03-28 23:37:17 +0200

    finish of deploying

--- flask_website/script1.py
+++ flask_website/script1.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/about/')
 def about():
     return render_template('about.html')
-
 
 
 if __name__ == '__main__':
